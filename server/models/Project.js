14f0b2f187880c0ce8818b46c278fcd90b3216b6
Gourav <gs4494193@gmail.com>
2026-03-22 12:02:31 +0530

    fix bootstrap icons

--- server/models/Project.js
+++ server/models/Project.js
@@ -37,3 +37,34 @@ const projectSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model('Project', projectSchema);
+
+
+// {
+//   "name": "portfolio-client",
+//   "private": true,
+//   "version": "0.0.0",
+//   "type": "module",
+//   "scripts": {
+//     "dev": "vite",
+//     "build": "vite build",
+//     "preview": "vite preview"
+//   },
+//   "dependencies": {
+    
+//     "axios": "^1.6.2",
+//     "bootstrap": "^5.3.2",
+//     "bootstrap-icons": "^1.11.2",
+//     "react": "^18.2.0",
+//     "react-bootstrap": "^2.9.1",
+//     "react-dom": "^18.2.0",
+//     "react-router-dom": "^6.20.1",
+//     "vite": "^5.4.21"
+//   },
+//   "devDependencies": {
+    
+//     "@types/react": "^18.2.43",
+//     "@types/react-dom": "^18.2.17",
+//     "@vitejs/plugin-react": "^4.2.1",
+    
+//   }
+// }
